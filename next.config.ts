0963c7738b3296e543771c5cdf95d1e78f14ee75
mubarak muhammad hamisu <hamisumubarak447@gmail.com>
2025-12-29 22:12:30 +0100

    fixing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-/*const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {
+  config options here 
 };
 
-export default nextConfig;*/
-/** @type {import('next').NextConfig} */
-
-
-const nextConfig: NextConfig = {
-  swcMinify: false,
-}
-
-export default nextConfig
+export default nextConfig;
