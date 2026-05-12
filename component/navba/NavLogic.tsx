878ee853c512b011927c6ccc158d4357fef7e26d
mubarak muhammad hamisu <hamisumubarak447@gmail.com>
2026-05-13 00:15:39 +0100

    fix component conflect

--- component/navba/NavLogic.tsx
+++ component/navba/NavLogic.tsx
@@ -1,17 +1,33 @@
 "use client"
+import { useEffect, useRef } from 'react'
 import NavLink from '@/component/navba/NavLink'
 import { NavLinkData } from '@/utils/LinkData'
 import HanBG from '@/component/navba/HanBG'
+import { useToggleStore } from '@/Store/toggleStore'
 
 const NavLogic = () => {
-    
+  const { isOpen, setOpen } = useToggleStore();
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  // Close menu when clicking outside the navbar wrapper
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleClickOutside = (e: MouseEvent) => {
+      if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isOpen, setOpen]);
+
   return (
-    <>
+    <div ref={wrapperRef} className='flex items-center w-full'>
       <NavLink LinkData={NavLinkData} />
-      <HanBG className='sm:hidden'/>
-
-    </>
-
+      <HanBG className='sm:hidden' />
+    </div>
   )
 }
 
